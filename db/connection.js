948f2c96d0076a9707e99c1edb6b6322afd870d9
sarangyogi <sarang.yogi@spit.ac.in>
2021-11-03 21:01:15 +0530

    confidential

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-// const dbURL="mongodb+srv://user:easypassword@nodecrash.x5lyq.mongodb.net/QuoteGenerator?retryWrites=true&w=majority";
 mongoose.connect(process.env.dbURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
